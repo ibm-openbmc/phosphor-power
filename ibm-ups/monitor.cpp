fd245a40cfbaea02719973fb021e90db239cb0f2
Shawn McCarney <46543807+smccarney@users.noreply.github.com>
2022-01-05 12:52:09 -0600

    ibm-ups: Initial version of UPS application (#1)
    Create an initial version of the ibm-ups application.  This application
    will eventually monitor an Uninterruptible Power Supply (UPS).
    
    The UPS must be connected to a USB port on the BMC using an IBM "System
    Port Converter Cable".  The application will eventually read the UPS
    status from the cable and publish it on D-Bus using the UPower
    org.freedesktop.UPower.Device interface.
    
    This initial commit contains the following:
    * Meson changes to build the new application
    * Minimal C++ implementation that publishes a D-Bus object with default
      property values
    
    The long-term BMC strategy for UPS support is to use the full UPower
    software package.  However, a short-term solution is required to support
    IBM systems.  For this reason, this application is being developed
    downstream.
    
    Signed-off-by: Shawn McCarney <shawnmm@us.ibm.com>
    Change-Id: I19357c6351ca56ae7a5181656915c6a2534dd7b3

---
+++ ibm-ups/monitor.cpp
@@ -0,0 +1,34 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "monitor.hpp"
+
+namespace phosphor::power::ibm_ups
+{
+
+/**
+ * D-Bus service/bus name for this application.
+ */
+constexpr auto serviceName = "xyz.openbmc_project.Power.IBMUPS";
+
+Monitor::Monitor(sdbusplus::bus::bus& bus, const sdeventplus::Event& event) :
+    bus{bus}, eventLoop{event}, ups{bus}
+{
+    // Obtain D-Bus service name
+    bus.request_name(serviceName);
+}
+
+} // namespace phosphor::power::ibm_ups
