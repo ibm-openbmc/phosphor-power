3d95f8cb9be9c45a00fbdacd70a1dab314558308
Shawn McCarney <shawnmm@us.ibm.com>
2022-01-19 12:04:02 -0600

    ibm-ups: Poll UPS device for current status
    Add support to the ibm-ups application to poll the UPS device for
    current status.
    
    The UPS is connected to the BMC using an IBM System Port Converter
    cable.  This cable has a serial connector on the UPS side and a USB
    connector on the BMC side.
    
    The following bits are read from the cable:
    * UPS On: Indicates whether UPS is present/functional.
    * Battery Low: Indicates whether the UPS battery level is low.
    * Utility Fail: Indicates whether a utility failure has occurred,
                    causing the UPS to provide power to the system.
    
    The UPS is polled once per second.  The same bit values must be read
    three times in a row before they are considered valid.  This provides
    de-glitching to ignore a transient event that caused invalid data.
    
    Once the bit values are considered valid, the corresponding D-Bus
    properties are updated.  These properties will be consumed by PLDM and
    the state manager application.
    
    Since the UPS is hot-pluggable, the ibm-ups application must handle the
    UPS presence changing at any time.  If an attempt to read from the cable
    fails three times in a row, the application will assume the UPS has been
    removed.
    
    Tested:
    * Polling
      * Verified polling occurs once per second
      * Verified polling does not occur if --no-poll command line option
        specified
    * UPS presence
      * Tested where UPS is not present when BMC reaches Ready state
      * Tested where UPS is present when BMC reaches Ready state
      * Tested where UPS is added while BMC is running
      * Tested where UPS is removed while BMC is running
      * Verified device path of the form /dev/ttyUSB* exists if UPS is
        present
    * Reading status
      * Verified device path is found when UPS is present
      * Verified character special file at device path can be opened, read,
        and closed
      * Verified that once the device is opened, the resulting file handle
        is reused on subsequent polls
      * Verified that same modem control bits must be read 3 consecutive
        times before they are considered valid (to provide de-glitching)
      * Verified that 3 consecutive read failures cause device to be closed
        since UPS may have been removed
      * Verified the UPS status bits were correctly obtained from the modem
        control bits that were read
        * UPS On
        * Battery Low
        * Utility Fail
    * D-Bus properties
      * Verified correct initial property values are set when ibm-ups
        application first starts
      * Verified that D-Bus properties are updated when UPS status changes
      * Verified that D-Bus properties are reset to their initial values if
        the UPS is removed
    * PLDM
      * Verified that the PLDM state sensor for the UPS is updated when the
        UPS status changes
    
    Signed-off-by: Shawn McCarney <shawnmm@us.ibm.com>
    Change-Id: I08b6ce0e06143cbafeb7f49f58c9b772e5efb6a3

--- ibm-ups/ups.cpp
+++ ibm-ups/ups.cpp
@@ -16,8 +16,14 @@
 
 #include "ups.hpp"
 
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
 namespace phosphor::power::ibm_ups
 {
+
+namespace fs = std::filesystem;
 
 /**
  * D-Bus object path for the UPS.
@@ -28,19 +34,245 @@ namespace phosphor::power::ibm_ups
  */
 constexpr auto objectPath = "/org/freedesktop/UPower/devices/ups_hiddev0";
 
-UPS::UPS(sdbusplus::bus::bus& bus) : DeviceObject{bus, objectPath, true}
-{
-    // Set D-Bus properties that do not have the correct default value.  Skip
-    // emitting D-Bus signals until the object has been fully created.
-    bool skipSignal{true};
-    type(device::type::Ups, skipSignal);
-    powerSupply(true, skipSignal);
-    state(device::state::FullyCharged, skipSignal);
-    isRechargeable(true, skipSignal);
-    batteryLevel(device::battery_level::Full, skipSignal);
+/**
+ * Directory where the UPS character device file should exist.
+ */
+const fs::path deviceDirectory = "/dev";
 
-    // Now emit signal that object has been created
-    emit_object_added();
+/**
+ * Expected prefix of the UPS character device file name.
+ */
+constexpr auto deviceNamePrefix = "ttyUSB";
+
+/**
+ * Number of consecutive device read errors before we close the device.
+ *
+ * These errors may indicate the UPS has been removed.
+ */
+constexpr unsigned short maxReadErrorCount{3};
+
+/**
+ * Number of consecutive device reads that must return the same modem bit values
+ * before the values are considered valid.
+ *
+ * This provides de-glitching to ignore a transient event where invalid data is
+ * read.
+ */
+constexpr unsigned short requiredMatchingReadCount{3};
+
+UPS::UPS(sdbusplus::bus::bus& bus) : DeviceObject{bus, objectPath}
+{
+    // Set D-Bus properties to initial values indicating the UPS is not present
+    initializeDBusProperties();
+}
+
+UPS::~UPS()
+{
+    try
+    {
+        if (isDeviceOpen())
+        {
+            closeDevice();
+        }
+    }
+    catch (...)
+    {
+        // Destructors must not throw exceptions
+    }
+}
+
+void UPS::refresh()
+{
+    try
+    {
+        // Open the UPS device if necessary
+        if (!isDeviceOpen())
+        {
+            if (!openDevice())
+            {
+                // Unable to open device; may not be present
+                return;
+            }
+        }
+
+        // Read current status from UPS device
+        readDevice();
+    }
+    catch (...)
+    {
+        // Ignore error in case UPS was removed during the actions above
+    }
+}
+
+void UPS::closeDevice()
+{
+    // Close file descriptor
+    close(fd);
+    fd = INVALID_FD;
+
+    // Clear data members used to open and read the device
+    devicePath.clear();
+    readErrorCount = 0;
+    matchingReadCount = 0;
+    prevModemBits = INVALID_MODEM_BITS;
+
+    // Set D-Bus properties to initial values indicating the UPS is not present
+    initializeDBusProperties();
+}
+
+bool UPS::findDevicePath()
+{
+    devicePath.clear();
+    try
+    {
+        // Loop through all entries in the directory where the file should exist
+        std::string entryName{};
+        for (auto const& entry : fs::directory_iterator{deviceDirectory})
+        {
+            // If entry has expected prefix, exists, and is character device
+            entryName = entry.path().filename().native();
+            if (entryName.starts_with(deviceNamePrefix) && entry.exists() &&
+                entry.is_character_file())
+            {
+                // Found UPS device path
+                devicePath = entry.path();
+                break;
+            }
+        }
+    }
+    catch (...)
+    {
+        // Ignore errors; UPS may have been added/removed during loop
+    }
+    return (!devicePath.empty());
+}
+
+void UPS::handleReadDeviceFailure()
+{
+    // Clear consecutive matching read count and previous modem bits
+    matchingReadCount = 0;
+    prevModemBits = INVALID_MODEM_BITS;
+
+    // Increment consecutive error count
+    if (readErrorCount < maxReadErrorCount)
+    {
+        ++readErrorCount;
+    }
+
+    // If we have reached the maximum number of read errors, close UPS device
+    if (readErrorCount >= maxReadErrorCount)
+    {
+        closeDevice();
+    }
+}
+
+void UPS::handleReadDeviceSuccess(int modemBits)
+{
+    // Clear consecutive read error count
+    readErrorCount = 0;
+
+    // Check if modem bits have changed since the previous read
+    if (modemBits != prevModemBits)
+    {
+        // Modem bits have changed; set matching read count to 1
+        matchingReadCount = 1;
+    }
+    else
+    {
+        // Modem bits have not changed.  Increment matching read count.
+        if (matchingReadCount < requiredMatchingReadCount)
+        {
+            ++matchingReadCount;
+        }
+
+        // If we have reached the required number of matching reads
+        if (matchingReadCount >= requiredMatchingReadCount)
+        {
+            // Get UPS status from modem bit values.
+            bool isOn = static_cast<bool>(modemBits & TIOCM_CAR);
+            bool isBatteryLow = static_cast<bool>(modemBits & TIOCM_CTS);
+            bool isUtilityFail = static_cast<bool>(modemBits & TIOCM_DSR);
+
+            // Update D-Bus properties with current UPS status
+            updateDBusProperties(isOn, isBatteryLow, isUtilityFail);
+        }
+    }
+
+    // Save the modem bit values for comparison during the next read
+    prevModemBits = modemBits;
+}
+
+void UPS::initializeDBusProperties()
+{
+    type(device::type::Ups);
+    powerSupply(true);
+    isPresent(false);
+    state(device::state::FullyCharged);
+    isRechargeable(true);
+    batteryLevel(device::battery_level::Full);
+}
+
+bool UPS::openDevice()
+{
+    bool wasOpened{false};
+
+    // Find UPS device path
+    if (findDevicePath())
+    {
+        // Open device path
+        int rc = open(devicePath.c_str(), O_RDONLY);
+        if (rc >= 0)
+        {
+            fd = rc;
+            wasOpened = true;
+        }
+    }
+
+    return wasOpened;
+}
+
+void UPS::readDevice()
+{
+    // Read modem bits from device driver
+    int modemBits{0};
+    int rc = ioctl(fd, TIOCMGET, &modemBits);
+    if (rc < 0)
+    {
+        handleReadDeviceFailure();
+    }
+    else
+    {
+        handleReadDeviceSuccess(modemBits);
+    }
+}
+
+void UPS::updateDBusProperties(bool isOn, bool isBatteryLow, bool isUtilityFail)
+{
+    // Set D-Bus IsPresent property.  isOn means UPS is present/functional.
+    isPresent(isOn);
+
+    // Set D-Bus State property
+    if (isUtilityFail)
+    {
+        // Utility failure is occurring.  UPS is providing power to the system.
+        state(device::state::Discharging);
+    }
+    else if (isBatteryLow)
+    {
+        // UPS is not providing power to the system, but the battery is low.
+        // Assume the battery is charging.
+        state(device::state::Charging);
+    }
+    else
+    {
+        // UPS is not providing power to the system, and battery is not low.
+        // Assume the battery is fully charged.
+        state(device::state::FullyCharged);
+    }
+
+    // Set D-Bus BatteryLevel property
+    batteryLevel(isBatteryLow ? device::battery_level::Low
+                              : device::battery_level::Full);
 }
 
 } // namespace phosphor::power::ibm_ups
